ee3a4d14c3d247c25ecc4ea8680c4c87959bdd7c
griffinmichl <griffinmichl@gmail.com>
2018-02-28 14:33:19 -0800

    Default to card view

--- src/app/reducers/compact.js
+++ src/app/reducers/compact.js
@@ -1,7 +1,7 @@
 import * as compactActions from 'app/actions/compact';
 import * as loginActions from 'app/actions/login';
 
-export const DEFAULT = true;
+export const DEFAULT = false;
 
 export default (state=DEFAULT, action={}) => {
   switch (action.type) {
